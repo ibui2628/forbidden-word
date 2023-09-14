5ad37f93fb5bb698faa576735779a9936021e328
ibui2628 <92432744+ibui2628@users.noreply.github.com>
2023-09-14 20:48:54 +0900

    Update to 2.0

--- main_script.js
+++ main_script.js
@@ -1,21 +1,27 @@
-var kdata = {};
-kdata.fw_data = { word: "", exclusion: "" };
-chrome.storage.local.get(kdata, function (getdata) {
-    var ch = true;
-    const bodydata = document.body.innerHTML;
-    const or = bodydata.match(new RegExp(getdata.fw_data.word, "g"));
-    const url = location.hostname;
-    var exc = getdata.fw_data.exclusion;
-    exc = exc.split('|')
-    console.log("hostname:" + url);
-    for (const nai of exc) {
-        console.log(nai);
-        if (url == nai) {
-            ch = false;
-        }
-    }
-    console.log(ch);
-    if (or != null && ch) {
-        document.body.innerHTML = '<div style="background:yellow;text-align:center;padding:5px;"><h1>表示をブロックしました</h1><p>このサイトは禁止ワードが含まれているため表示をブロックしました</p><p>検出された禁止ワード:' + or + '</p><br>アクセスしたい場合はこの拡張機能を無効化してください</p><a class="19clm1dnxr" href="javascript:history.back();">戻る</a></div>';
-    }
-});
+window.addEventListener("DOMContentLoaded", function() {
+    window.setTimeout(function(){
+        var kdata = {};
+        kdata.fw_data = { word: "", exclusion: "" };
+        chrome.storage.local.get(kdata, function (getdata) {
+            const url = location.hostname;
+            console.log("hostname:" + url);
+            if (getdata.fw_data.word != '') {
+                var ch = true;
+                const bodydata = document.body.innerHTML;
+                const or = bodydata.match(new RegExp(getdata.fw_data.word, "g"));
+                var exc = getdata.fw_data.exclusion;
+                exc = exc.split('|')
+                for (const nai of exc) {
+                    if (url == nai) {
+                        ch = false;
+                    }
+                }
+                if (or != null && ch) {
+                    document.body.innerHTML = '<style>html{background:yellow;font-family:sans-serif;color:black;}</style><div style="background:yellow;text-align:center;padding:5px;"><h1>表示をブロックしました</h1><p>以下のサイトはは禁止ワードが含まれているため表示をブロックしました</p><p>' + location.href + '</p><p>検出された禁止ワード:"' + or + '"</p><br>アクセスしたい場合はこの拡張機能を無効化してください</p><a class="19clm1dnxr" href="javascript:history.back();"><<戻る</a></div>';
+                }
+            } else {
+                console.warn("forbidden-word off");
+            }
+        });
+    }, 1000);
+})
