09022dca09bf51fbd93639d295d35d95c6508d92
ibui2628 <92432744+ibui2628@users.noreply.github.com>
2023-09-14 21:20:20 +0900

    Fixed a bug where sites were not blocked

--- main_script.js
+++ main_script.js
@@ -1,27 +1,25 @@
-window.addEventListener("DOMContentLoaded", function() {
-    window.setTimeout(function(){
-        var kdata = {};
-        kdata.fw_data = { word: "", exclusion: "" };
-        chrome.storage.local.get(kdata, function (getdata) {
-            const url = location.hostname;
-            console.log("hostname:" + url);
-            if (getdata.fw_data.word != '') {
-                var ch = true;
-                const bodydata = document.body.innerHTML;
-                const or = bodydata.match(new RegExp(getdata.fw_data.word, "g"));
-                var exc = getdata.fw_data.exclusion;
-                exc = exc.split('|');
-                for (const nai of exc) {
-                    if (url == nai) {
-                        ch = false;
-                    }
+const url = location.hostname;
+console.log("hostname:" + url);
+window.setTimeout(function () {
+    var kdata = {};
+    kdata.fw_data = { word: "", exclusion: "" };
+    chrome.storage.local.get(kdata, function (getdata) {
+        if (getdata.fw_data.word != '') {
+            var ch = true;
+            const bodydata = document.body.innerHTML;
+            const or = bodydata.match(new RegExp(getdata.fw_data.word, "g"));
+            var exc = getdata.fw_data.exclusion;
+            exc = exc.split('|');
+            for (const nai of exc) {
+                if (url == nai) {
+                    ch = false;
                 }
-                if (or != null && ch) {
-                    document.body.innerHTML = '<style>html{background:yellow;font-family:sans-serif;color:black;}</style><div style="background:yellow;text-align:center;padding:5px;"><h1>表示をブロックしました</h1><p>以下のサイトはは禁止ワードが含まれているため表示をブロックしました</p><p>' + location.href + '</p><p>検出された禁止ワード:"' + or + '"</p><br>アクセスしたい場合はこの拡張機能を無効化してください</p><a class="19clm1dnxr" href="javascript:history.back();"><<戻る</a></div>';
-                }
-            } else {
-                console.log("forbidden-word off");
             }
-        });
-    }, 300);
-});
+            if (or != null && ch) {
+                document.body.innerHTML = '<style>html{background:yellow;font-family:sans-serif;color:black;}</style><div style="background:yellow;text-align:center;padding:5px;"><h1>表示をブロックしました</h1><p>以下のサイトはは禁止ワードが含まれているため表示をブロックしました</p><p>' + location.href + '</p><p>検出された禁止ワード:"' + or + '"</p><br>アクセスしたい場合はこの拡張機能を無効化してください</p><a class="19clm1dnxr" href="javascript:history.back();"><<戻る</a></div>';
+            }
+        } else {
+            console.log("forbidden-word off");
+        }
+    });
+}, 500);
