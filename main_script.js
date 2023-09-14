84c0ddfe9e696fe1e4ffedc75d35a220d5a192c4
ibui2628 <92432744+ibui2628@users.noreply.github.com>
2023-09-14 21:07:21 +0900

    small fix

--- main_script.js
+++ main_script.js
@@ -10,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function() {
                 const bodydata = document.body.innerHTML;
                 const or = bodydata.match(new RegExp(getdata.fw_data.word, "g"));
                 var exc = getdata.fw_data.exclusion;
-                exc = exc.split('|')
+                exc = exc.split('|');
                 for (const nai of exc) {
                     if (url == nai) {
                         ch = false;
@@ -20,8 +20,8 @@ window.addEventListener("DOMContentLoaded", function() {
                     document.body.innerHTML = '<style>html{background:yellow;font-family:sans-serif;color:black;}</style><div style="background:yellow;text-align:center;padding:5px;"><h1>表示をブロックしました</h1><p>以下のサイトはは禁止ワードが含まれているため表示をブロックしました</p><p>' + location.href + '</p><p>検出された禁止ワード:"' + or + '"</p><br>アクセスしたい場合はこの拡張機能を無効化してください</p><a class="19clm1dnxr" href="javascript:history.back();"><<戻る</a></div>';
                 }
             } else {
-                console.warn("forbidden-word off");
+                console.log("forbidden-word off");
             }
         });
-    }, 1000);
-})
+    }, 300);
+});
