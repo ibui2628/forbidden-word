d33382aed069e95aec769bb88a424aa0dad34aa5
ibui2628 <92432744+ibui2628@users.noreply.github.com>
2024-03-27 21:02:25 +0900

    2.3.0 Update Changed logo and added strict judgment

--- main_script.js
+++ main_script.js
@@ -1,27 +1,38 @@
 const url = location.hostname;
-console.log("hostname:" + url);
+console.log('hostname:' + url);
+
+const cehk = (g_data) => {
+    //console.log(g_data);
+    var ch = true;
+    const bodydata = document.body.innerHTML;
+    const or = bodydata.match(new RegExp(g_data.word, "g"));
+    var exc = g_data.exclusion;
+    exc = exc.split('|');
+    for (const nai of exc) {
+        if (url == nai) {
+            ch = false;
+        }
+    }
+    if (or != null && ch) {
+        document.body.innerHTML = '<style>html,body{background:yellow!important; font-family:sans-serif; color:black!important;}</style><div style="background:yellow;text-align:center;padding:5px;"><h1>表示をブロックしました</h1><p>以下のサイトはは禁止ワードが含まれているため表示をブロックしました</p><p>' + location.href + '</p><p>検出された禁止ワード:"' + or + '"</p><br>アクセスしたい場合はこの拡張機能を無効化してください</p><a class="19clm1dnxr" href="javascript:history.back();"><<戻る</a></div>';
+        g_data.rigor = 'none';
+    }
+    if (g_data.rigor == 'low') {
+        window.setTimeout(() => {
+            cehk(g_data);
+        }, 5000);
+    }
+}
+
 window.setTimeout(function () {
     var kdata = {};
-    kdata.fw_data = { word: "", exclusion: ""};
+    kdata.fw_data = { word: '', exclusion: '', rigor: 'none' };
     kdata.power = true;
     chrome.storage.local.get(kdata, function (getdata) {
-
         if (getdata.fw_data.word != '' && getdata.power) {
-            var ch = true;
-            const bodydata = document.body.innerHTML;
-            const or = bodydata.match(new RegExp(getdata.fw_data.word, "g"));
-            var exc = getdata.fw_data.exclusion;
-            exc = exc.split('|');
-            for (const nai of exc) {
-                if (url == nai) {
-                    ch = false;
-                }
-            }
-            if (or != null && ch) {
-                document.body.innerHTML = '<style>html{background:yellow;font-family:sans-serif;color:black;}</style><div style="background:yellow;text-align:center;padding:5px;"><h1>表示をブロックしました</h1><p>以下のサイトはは禁止ワードが含まれているため表示をブロックしました</p><p>' + location.href + '</p><p>検出された禁止ワード:"' + or + '"</p><br>アクセスしたい場合はこの拡張機能を無効化してください</p><a class="19clm1dnxr" href="javascript:history.back();"><<戻る</a></div>';
-            }
+            cehk(getdata.fw_data);
         } else {
-            console.log("forbidden-word off");
+            console.log('forbidden-word off');
         }
     });
 }, 500);
--- pages/options_script.js
+++ pages/options_script.js
@@ -1,22 +1,25 @@
 //var a = chrome.runtime.id;
 const noword = document.getElementById("noword");
 const noexclusion = document.getElementById("noexclusion");
+const rigor = document.getElementById("rigor");
 document.getElementById("ge").addEventListener("click", ge);
 
 var ydata = {};
-ydata.fw_data = { word: "", exclusion: "" };
+ydata.fw_data = { word: '', exclusion: '', rigor: 'none' };
 chrome.storage.local.get(ydata, function (getdata) {
     console.log("word:" + getdata.fw_data.word);
     console.log("exclusion:" + getdata.fw_data.exclusion);
     noword.value = getdata.fw_data.word;
     noexclusion.value = getdata.fw_data.exclusion;
+    rigor.value = getdata.fw_data.rigor;
 });
 
 function ge() {
     var kdata = {};
     const word = noword.value;
     const noexclusion2 = noexclusion.value;
-    kdata.fw_data = { word: word, exclusion: noexclusion2 };
+    const rigord = rigor.value;
+    kdata.fw_data = {word: word, exclusion: noexclusion2, rigor: rigord};
     chrome.storage.local.set(kdata, function () {
         alert("設定を反映しました");
     });
